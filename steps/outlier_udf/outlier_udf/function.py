b99c202a24b1fe9be0de8cdfd56572e403c7ce78
PoorvikaGirishBabu <girishbabu.p@northeastern.edu>
2025-02-26 21:35:05 -0500

    modified comments to the python UDF File

--- steps/outlier_udf/outlier_udf/function.py
+++ steps/outlier_udf/outlier_udf/function.py
@@ -1,13 +1,3 @@
-#------------------------------------------------------------------------------
-# Hands-On Lab: Data Engineering with Snowpark
-# Script:       05_fahrenheit_to_celsius_udf/app.py
-# Author:       Jeremiah Hansen, Caleb Baechtold
-# Last Updated: 1/9/2023
-#------------------------------------------------------------------------------
-
-# SNOWFLAKE ADVANTAGE: Snowpark Python programmability
-# SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
-# SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
 import sys
 
@@ -20,12 +10,12 @@ def main(TMAX: float, TMIN: float) -> str:
     tmin (float): The minimum temperature for a given date in Fahrenheit.
 
     Returns:
-    float: A combined score indicating outlier status (1 for outliers, 0 for normal).
+    String: Mentions if TMIN or TMAX is an outlier.
     """
     
     # Predefined ranges in Fahrenheit
-    TMAX_min, TMAX_max = 68, 104  # Normal range for tmax (68°F to 104°F)
-    TMIN_min, TMIN_max = 14, 59  # Normal range for tmin (14°F to 59°F)
+    TMAX_min, TMAX_max = 68, 104  
+    TMIN_min, TMIN_max = 14, 59  
     
     # Check if tmax is outside the normal range (outlier)
     TMAX_outlier = not (TMAX_min <= TMAX <= TMAX_max)
