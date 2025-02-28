3ad9e6791f312d7b10e2cb3d6ca4c2d68296202b
Lenin Kumar Gorle <gorle.l@northeastern.edu>
2025-02-27 20:53:41 -0500

    modified ingest file

--- steps/ingest_data_to_s3.py
+++ steps/ingest_data_to_s3.py
@@ -16,7 +16,7 @@ s3_client = boto3.client(
     "s3",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION"),
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 zip_codes = ["02128", "02130", "02186", "02766", "01602"]
